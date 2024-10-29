d42c2e7d18ce82b26d9dc276940768a27cb3fa8f
Hillman-gaetan <gaetan.bergmans@gmail.com>
2024-10-29 14:44:38 +0100

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -6,7 +6,7 @@ if [[ $# = 0 ]]; then
     exit 1
 fi
 #creation of secure password using openssl
-password="soleil1234"
+password="defaultpassword"
 hashed_password=$(openssl passwd "$password")
 
 # loop through each argument
